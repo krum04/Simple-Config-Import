869d74e8ccaf79b4f15a25a329948a961cf9f27c
krum04 <krum04@gmail.com>
2022-11-02 19:31:21 -0500

    added support for line commenting and removed whitespace from variables

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Simple Config Import
 
 This script will read an external config.txt and import your values as global variables in a program.
+Lines can be commented out from the config text using the pound sign #
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-# Pass config text argument to set global variables
+# Pass config.txt into function to set global variables 
 def importConfig(file,printVars=False):
     with open(file) as f:
         lines = f.readlines()
         myVars = globals()
         for line in lines:
+            if line[:1]=='#':
+                continue
             if printVars == True:
                 print(line.replace('\n',''))
-            myVars[line.split(' ')[0]] = line.split(' ')[2]
+            myVars[(line.split(' ')[0]).strip()] = (line.split(' ')[2]).strip()
